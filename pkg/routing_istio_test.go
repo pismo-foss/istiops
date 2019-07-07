f507f6d4e0e9d1ff9952553cde1906f18fb6c4d4
Victor Santos <vsantos.py@gmail.com>
2019-07-07 13:39:15 -0300

    fix identation

--- pkg/routing_istio_test.go
+++ pkg/routing_istio_test.go
@@ -16,8 +16,16 @@ func TestGenerateShaFromMap(t *testing.T) {
 	values, err := GenerateShaFromMap(mapToHash)
 
 	assert.NoError(t, err)
-	assert.Equal(t, fmt.Sprintf("%s", values[0]), "563f0357118d05ef145d6bddf2966cc23e86ca8f2f013f915e565afdf09f7a23")
-	assert.Equal(t, fmt.Sprintf("%s", values[1]), "dc705ce67cc0d8a71c4449a2933fb0ac0404e111325ee7a08c27f2a17fe4a9e2")
+	assert.Equal(
+		t,
+		fmt.Sprintf("%s", values[0]),
+		"563f0357118d05ef145d6bddf2966cc23e86ca8f2f013f915e565afdf09f7a23",
+	)
+	assert.Equal(
+		t,
+		fmt.Sprintf("%s", values[1]),
+		"dc705ce67cc0d8a71c4449a2933fb0ac0404e111325ee7a08c27f2a17fe4a9e2",
+	)
 }
 
 func TestCompareMapsKeyPairsHash(t *testing.T) {
